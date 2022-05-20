8bc3862ac28d2758ad36357dc994a69e7d338cd1
Konstantin <opezdalya@gmail.com>
2022-05-20 20:19:15 +0300

    Delete psvm file

--- src/main/java/Sort/Main.java
+++
@@ -1,9 +0,0 @@
-package Sort;
-
-public class Main {
-    public static void main(String[] args) {
-        BubleSort bubleSort = new BubleSort();
-        int[] arr = {1,3,45,2};
-        System.out.println(bubleSort.sortAlghtorim(arr));
-    }
-}
